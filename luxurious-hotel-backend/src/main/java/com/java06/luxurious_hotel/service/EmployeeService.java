4c320fcb0cfe170f2d0ec5ce364377f0c3c53880
Doan Minh Thai <thaidoan2212@gmail.com>
2024-09-07 01:36:35 +0700

    lay user theo id va lay user theo role

--- luxurious-hotel-backend/src/main/java/com/java06/luxurious_hotel/service/EmployeeService.java
+++ luxurious-hotel-backend/src/main/java/com/java06/luxurious_hotel/service/EmployeeService.java
@@ -1,13 +1,20 @@
 package com.java06.luxurious_hotel.service;
 
 import com.java06.luxurious_hotel.dto.EmployeeDTO;
+import com.java06.luxurious_hotel.dto.RoleDTO;
 import com.java06.luxurious_hotel.request.AddEmployeeRequest;
 import com.java06.luxurious_hotel.request.UpdateEmployeeRequest;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public interface EmployeeService {
     void addEmployee(AddEmployeeRequest addEmployeeRequest);
     Boolean updateEmployee(UpdateEmployeeRequest updateEmployeeRequest);
     Boolean deleteEmployee(int employeeId);
+    EmployeeDTO getEmployee(int employeeId);
+    Map<RoleDTO, List<EmployeeDTO>> getAllEmployee();
 }
+
