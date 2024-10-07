42275b84f92a946c971bc96d4765af8b72e9b5d5
Trung Nguyen <nguyentrung875@gmail.com>
2024-10-07 09:59:20 +0700

    finish edit send booking email

--- luxurious-hotel-backend/src/main/java/com/java06/luxurious_hotel/service/EmailService.java
+++ luxurious-hotel-backend/src/main/java/com/java06/luxurious_hotel/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.java06.luxurious_hotel.service;
 
+import com.java06.luxurious_hotel.dto.BookingDTO;
 import com.java06.luxurious_hotel.entity.BookingEntity;
 import com.java06.luxurious_hotel.entity.RoomBookingEntity;
 import com.java06.luxurious_hotel.request.AddBookingRequest;
@@ -13,5 +14,7 @@ import java.util.List;
 public interface EmailService {
     String sendEmail(String recipients, String subject, String content, MultipartFile[] files) throws MessagingException;
     String sendConfirmBookingEmail(int idBooking) throws MessagingException;
-    String sendSuccessfulBookingEmailToQueue(int idBooking);
+    String sendSuccessfulBookingEmailToQueue(BookingDTO bookingDTO);
+    String sendConfirmBookingEmailToQueue(AddBookingRequest bookingDTO);
+
 }
